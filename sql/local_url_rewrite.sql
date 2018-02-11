1a5f14bbcc8a5fb696131e7f2c46a5e1234797c7
eslater <eric9970@sbcglobal.net>
2018-02-10 19:08:14 -0600

    cleanup unused files

--- sql/local_url_rewrite.sql
+++ sql/local_url_rewrite.sql
@@ -1,5 +1,5 @@
-UPDATE wp_options SET option_value = replace(option_value, 'http://ricochetconstruction.com', 'http://localhost') WHERE option_name = 'home' OR option_name = 'siteurl';
-UPDATE wp_posts SET guid = replace(guid, 'http://ricochetconstruction.com', 'http://localhost');
-UPDATE wp_posts SET post_content = replace(post_content, 'http://ricochetconstruction.com', 'http://localhost');
-UPDATE wp_postmeta SET meta_value = replace(meta_value,'http://ricochetconstruction.com', 'http://localhost');
+UPDATE wp_options SET option_value = replace(option_value, 'http://test.com', 'http://localhost') WHERE option_name = 'home' OR option_name = 'siteurl';
+UPDATE wp_posts SET guid = replace(guid, 'http://test.com', 'http://localhost');
+UPDATE wp_posts SET post_content = replace(post_content, 'http://test.com', 'http://localhost');
+UPDATE wp_postmeta SET meta_value = replace(meta_value,'http://test.com', 'http://localhost');
 commit;
--- sql/prod_url_rewrite.sql
+++ sql/prod_url_rewrite.sql
@@ -1,5 +1,5 @@
-UPDATE wp_options SET option_value = replace(option_value, 'http://localhost', 'http://ricochetconstruction.com') WHERE option_name = 'home' OR option_name = 'siteurl';
-UPDATE wp_posts SET guid = replace(guid, 'http://localhost', 'http://ricochetconstruction.com');
-UPDATE wp_posts SET post_content = replace(post_content, 'http://localhost', 'ricochetconstruction.com');
-UPDATE wp_postmeta SET meta_value = replace(meta_value, 'http://localhost', 'ricochetconstruction.com');
+UPDATE wp_options SET option_value = replace(option_value, 'http://localhost', 'http://test.com') WHERE option_name = 'home' OR option_name = 'siteurl';
+UPDATE wp_posts SET guid = replace(guid, 'http://localhost', 'http://test.com');
+UPDATE wp_posts SET post_content = replace(post_content, 'http://localhost', 'http://test.com');
+UPDATE wp_postmeta SET meta_value = replace(meta_value, 'http://localhost', 'http://test.com');
 commit;
