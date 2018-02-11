0c889bd5905069ad710f439868a42132f7d5229f
eslater <eric9970@sbcglobal.net>
2018-02-10 19:00:53 -0600

    beef up create_site.sh

--- create_site.sh
+++ create_site.sh
@@ -8,17 +8,45 @@ docker image prune -f
 docker volume prune -f
 docker-compose -f docker/docker-compose.yml build
 docker-compose -f docker/docker-compose.yml up -d
-sleep 30
 
+timeout=$((SECONDS+30))
+check_mysql() {
+	docker exec docker_mariadb_1 /bin/sh -c 'mysql -u root -proot demo_db && echo "pass" || echo "fail"'
+}
+
+until [ "$(check_mysql)" == "pass" ] || [ "$SECONDS" -gt "$timeout" ]; do
+    printf "Waiting for mysql to boot.....\n"
+    sleep 2
+done
+
+if [ "$(check_mysql)" != "pass" ]; then 
+  printf "mysql failed to boot!\n"
+  exit 1
+else
+  printf "mysql booted!\n"
+fi
+
+printf "loading data in to container...\n"
 docker cp sql/initdata.sql docker_mariadb_1:/
+
+printf "importing into database...\n"
 docker exec docker_mariadb_1 /bin/sh -c 'mysql -u root -proot demo_db </initdata.sql'
 
-#docker cp sql/truncate_tables.sql docker_mariadb_1:/
-#docker cp sql/create_wp_users.sql docker_mariadb_1:/
-#docker cp sql/create_wp_usermeta.sql docker_mariadb_1:/
-#docker cp sql/local_url_rewrite.sql docker_mariadb_1:/
+timeout=$((SECONDS+30))
+check_http() {
+	curl -A "Web Check" -sL --connect-timeout 3 -w "%{http_code}\n" "http://127.0.0.1" -o /dev/null
+}
 
-#docker exec docker_mariadb_1 /bin/sh -c 'mysql -u root -proot demo_db </truncate_tables.sql'
-#docker exec docker_mariadb_1 /bin/sh -c 'mysql -u root -proot demo_db </create_wp_users.sql'
-#docker exec docker_mariadb_1 /bin/sh -c 'mysql -u root -proot demo_db </create_wp_usermeta.sql'
-#docker exec docker_mariadb_1 /bin/sh -c 'mysql -u root -proot demo_db </local_url_rewrite.sql'
+until [ "$(check_http)" == "200" ] || [ "$SECONDS" -gt "$timeout" ]; do
+    printf "Waiting for site to boot.....\n"
+    sleep 2
+done
+
+if [ "$(check_http)" != "200" ]; then 
+  printf "Site failed to boot!\n"
+  exit 1
+else
+  printf "Site booted!\n"
+  exit 0
+fi
+
